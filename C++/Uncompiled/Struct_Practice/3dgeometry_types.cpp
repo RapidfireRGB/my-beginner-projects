531f3b00dd9b5d5f5d11596904feeaac6f5e023f
RapidfireRGB <rapidfirerpg@gmail.com>
2025-12-28 18:45:50 -0500

    Refactor 3dgeometry_types.cpp to resolve errors

--- C++/Uncompiled/Struct_Practice/3dgeometry_types.cpp
+++ C++/Uncompiled/Struct_Practice/3dgeometry_types.cpp
@@ -1,53 +1,63 @@
 #include <iostream>
 #include <cmath>
-const double pi = 3.14;
+constexpr double pi = 3.14;
 
-struct 3dshape {
+struct threedimensional {
 	double surface_area;
 	double volume;
 
-	[[nodiscard]] double surf() {
+	// Child classes will use their respective formulas here for constructor inputs.
+	threedimensional(double sa, double v) : surface_area(sa), volume(v) {
+	}
+
+	virtual ~threedimensional() = default;
+
+	[[nodiscard]] double surf() const {
         return surface_area;
     }
 
-    [[nodiscard]] double vol() {
+    [[nodiscard]] double vol() const {
         return volume;
     }
 
-	void dimensions() const {
+	virtual void dimensions() const {
 		std::cout << "Surface Area: " << surface_area << "\n";
 		std::cout << "Volume: " << volume << "\n";
 	}
 
 };
 
-struct sphere : 3dshape {
+struct sphere final : threedimensional {
     double radius;
     double diameter;
 	double circumference;
 
 	// Default sphere with a radius of 1.
-	sphere() {
+	sphere() : threedimensional(12.57, 4.19){
 		radius = 1;
 		diameter = 2;
 		circumference = 6.28;
 	}
 
 	// Only need to define a radius to construct a sphere.
-	sphere(double r) : radius(r) {
+    explicit sphere(const double r) : threedimensional(
+		(4 * pi) * (r * r),
+		(4 * pi)/3 * (r * r * r)) {
+
+		radius = r;
+
 		if (r <= 0) {
 			std::cerr << "Radius cannot be negative. Radius will default to 1; Diameter will default to 2.\n";
 			radius = 1;
-
+			surface_area = 12.57;
+			volume = 4.19;
 		}
-		diameter = r * 2;
+
+		diameter = radius * 2;
 		circumference = pi * diameter;
 	}
 
-	~sphere() = default;
-
-	volume = (4 * pi)/3 * (radius * radius * radius);
-	surface_area = (4 * pi) * (radius * radius);
+	~sphere() override = default;
 
 	[[nodiscard]] double r() {
         return radius;
@@ -69,25 +79,38 @@ struct sphere : 3dshape {
 		std::cout << "Circumference: " << circumference << "\n";
 	}
 
+	//void formulae() {
+	// Print relevant formulas for this shape
+	//}
+
 };
 
-struct cube : 3dshape {
+struct cube final : threedimensional {
 	double sidelength;
 	double face;
 
 	// Default cube with a side length of 1.
-	cube() {
+	cube() : threedimensional(6, 1){
 		sidelength = 1;
+		face = 1;
 	}
 
-	cube(double length) : sidelength(length) {
+	explicit cube(const double length) : threedimensional(
+		6 * (length * length),
+		(length * length * length)) {
+
+		sidelength = length;
+
+		if (length <= 0) {
+			std::cerr << "Sidelength cannot be negative. Sidelength will default to 1.\n";
+			sidelength = 1;
+			surface_area = 6;
+			volume = 1;
+		}
+		face = sidelength * sidelength;
 	}
 
-	~cube() = default;
-
-	face = sidelength * sidelength;
-	surface_area = face * 6;
-	volume = (sidelength * sidelength * sidelength);
+	~cube() override = default;
 
 	// Prints info of a cube. Type void.
 	void dimensions() const override {
@@ -98,7 +121,7 @@ struct cube : 3dshape {
 	}
 };
 
-struct cylinder : 3dshape {
+struct cylinder final : threedimensional {
 	double radius;
 	double diameter;
 	double height;
@@ -107,36 +130,48 @@ struct cylinder : 3dshape {
 	double wrap_area;
 
 	// Default Cylinder with a radius of 1 and a height of 1.
-	cylinder() {
+	cylinder() : threedimensional(12.57, 3.14){
 		radius = 1;
 		diameter = 2;
 		height = 1;
 		circumference = 6.28;
 		base_area = 3.14;
-		wrap_area = 6.28
+		wrap_area = 6.28;
 	}
 
 	// Need to define radius and height to define a cylinder.
-	cylinder(double r, double h) : radius(r), height(h) {
+	cylinder(const double r, const double h) : threedimensional(
+		2 * (pi * (r * r)) + h * (2 * pi * r),
+		pi * (r * r) * h) {
+
+		radius = r;
+		height = h;
+		bool has_valid_dimensions = true;
 		if (r <= 0) {
 			std::cerr << "Radius cannot be negative. Radius will default to 1; Diameter will default to 2.\n";
 			radius = 1;
-
+			has_valid_dimensions = false;
 		}
+
 		if (h <= 0) {
 			std::cerr << "Height cannot be negative. Height will default to 1.\n";
-			h = 1;
+			height = 1;
+			has_valid_dimensions = false;
 		}
-		diameter = 2 * r;
+
+		// If invalid values are given for r or h, recalculate surface area and volume.
+		if (!has_valid_dimensions) {
+			surface_area = 2 * (pi * (radius*radius)) + height * (2 * pi * radius);
+			volume = pi * (radius * radius) * height;
+		}
+
+		diameter = radius * 2;
 		circumference = pi * diameter;
 		base_area = pi * (radius * radius);
 		wrap_area = circumference * height;
 	}
 
-	~cylinder() = default;
-
-	surface_area = (base_area * 2) + wrap_area;
-	volume = base_area * height;
+	~cylinder() override = default;
 
 	void dimensions() const override {
 		std::cout << "Surface Area: " << surface_area << "\n";
@@ -146,12 +181,11 @@ struct cylinder : 3dshape {
 		std::cout << "Circumference: " << circumference << "\n";
 		std::cout << "Surface Area of a Base: " << base_area << "\n";
 		std::cout << "Surface Area of Cross Section: " << wrap_area << "\n";
-
 	}
 
 };
 
-struct cone : 3dshape {
+struct cone final : threedimensional {
 	double radius;
 	double diameter;
 	double height;
@@ -160,35 +194,49 @@ struct cone : 3dshape {
 	double base_area;
 
 	// Default cone with a radius of 1 and height of 1.
-	cone() {
+	cone() : threedimensional(7.58, 1.05) {
 		radius = 1;
 		diameter = 2;
 		height = 1;
+		slant_height = sqrt((height * height) + (radius * radius));
 		base_circumference = 6.28;
 		base_area = 3.14;
 	}
 
 	// Need to define a radius and height to define a cone.
-	cone(double r, double h) : radius(r), height(h) {
+	cone(const double r, const double h) : threedimensional(
+		pi * r * (r + sqrt((h*h) + (r*r))) + pi * (r * r),
+		(pi * (r * r) * h)/3) {
+
+		radius = r;
+		height = h;
+		bool has_valid_dimensions = true;
+
 		if (r <= 0) {
 			std::cerr << "Radius cannot be negative. Radius will default to 1; Diameter will default to 2.\n";
 			radius = 1;
+			has_valid_dimensions = false;
+		}
 
-		}
 		if (h <= 0) {
-			std::cerr "Height cannot be negative. Height will default to 1.\n";
+			std::cerr << "Height cannot be negative. Height will default to 1.\n";
 			height = 1;
+			has_valid_dimensions = false;
 		}
-		diameter = r * 2;
+
+		slant_height = sqrt((height * height) + (radius * radius));
+
+		// If invalid values are given for r or h, recalculate surface area and volume.
+		if (!has_valid_dimensions) {
+			surface_area = pi * radius * (radius + slant_height) + pi * (radius * radius);
+			volume = (pi * (radius * radius) * height) / 3;
+		}
+		diameter = radius * 2;
 		base_circumference = pi * diameter;
 		base_area = pi * (radius * radius);
 	}
 
-	~cone() = default;
-
-	slant_height = std::sqrt((height * height) + (radius * radius));
-	surface_area = pi * radius * (radius + slant_height);
-	volume = (pi * (radius * radius) * height) / 3;
+	~cone() override = default;
 
 	void dimensions() const override {
 		std::cout << "Surface Area: " << surface_area << "\n";
@@ -203,37 +251,49 @@ struct cone : 3dshape {
 
 };
 
-struct rectangularprism : 3dshape {
+struct rectangularprism final : threedimensional {
 	double length;
 	double width;
 	double height;
 
 	// Default rect prism has l=1; w=2; h=3
-	rectangularprism() {
+	rectangularprism() : threedimensional(22, 6){
 		length = 1;
 		width = 2;
 		height = 3;
 	}
 
-	rectangularprism(double l, double w, double h) : length(l), width(w), height(h) {
+	rectangularprism(const double l, const double w, const double h) : threedimensional(
+		2 * ((l * w) + (l * h) + (w * h)),
+		l * w * h) {
+
+		length = l;
+		width = w;
+		height = h;
+		bool has_valid_dimensions = true;
+
 		if (l <= 0) {
 			std::cerr << "Length cannot be negative. Length will default to 1.\n";
 			length = 1;
+			has_valid_dimensions = false;
 		}
 		if (w <= 0) {
 			std::cerr << "Width cannot be negative. Width will default to 2.\n";
 			width = 2;
+			has_valid_dimensions = false;
 		}
 		if (h <= 0) {
 			std::cerr << "Height cannot be negative. Height will default to 3.\n";
 			height = 3;
+			has_valid_dimensions = false;
+		}
+		if (!has_valid_dimensions) {
+			surface_area = 2 * ((length * width) + (length * height) + (width * height));
+			volume = length * width * height;
 		}
 	}
 
-	~rectangularprism() = default;
-
-	surface_area = 2 * ((length * width) + (length * height) + (width * height));
-	volume = length * width * height;
+	~rectangularprism() override = default;
 
 	void dimensions() const override {
 		std::cout << "Surface Area: " << surface_area << "\n";
@@ -241,16 +301,15 @@ struct rectangularprism : 3dshape {
 		std::cout << "Length: " << length << "\n";
 		std::cout << "Width: " << width << "\n";
 		std::cout << "Height: " << height << "\n";
-
 	}
 };
 
-struct triangularprism : 3dshape {
+struct triangularprism : threedimensional {
 
 
 };
 
-struct pyramid : 3dshape {
+struct pyramid : threedimensional {
 
 
 };
